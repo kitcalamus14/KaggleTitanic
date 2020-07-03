340a903c76abf509509654266b60e22ebc743b36
kitcalamus14 <67679262+kitcalamus14@users.noreply.github.com>
2020-07-03 16:01:48 +0800

    Preliminary Analysis Initiated

--- TitanicCode.py
+++ TitanicCode.py
@@ -27,32 +27,78 @@ testset1 = pd.read_csv('test.csv')
 #proposed steps 4. remove identified useless variable 1 by 1, loop and compare result
 #proposed steps 5. finalize selected variable, output result for testing
 
+#Preliminary Analysis /EDA
+##Descriptive Statistics
+
+colname = list(dat1.columns.values)
+colname
+colname.insert(0,'Stat')
+DescriptiveStat = pd.DataFrame(columns = colname)
+missingval = dat1.isnull().sum(axis=0)
+mean = dat1.mean()
 
 #preliminary ML
-
+#Data Cleansing and XY split
 del dat1['Name']
-
-
-
-
-
-
-
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import train_test_split
-
-
+count = dat1.iloc[:,4].isna().sum()
+dat1 = dat1.dropna(subset=['Age'])
+dat1 = dat1.dropna(subset=['Embarked'])
 X = dat1.copy()
 del X['Survived']
+del X['Ticket']
+del X['Cabin']
+X['Sex'].replace('male',1,inplace=True)
+X['Sex'].replace('female',2,inplace=True)
+X['Embarked'].replace('C',1,inplace=True)
+X['Embarked'].replace('Q',2,inplace=True)
+X['Embarked'].replace('S',3,inplace=True)
 y = dat1['Survived']
 
+
+
+#NB MAIN CODE and Test/train split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.5, random_state=0)
 gnb = GaussianNB()
-y_pred = gnb.fit()
+y_pred = gnb.fit(X_train, y_train)
+y_pred = gnb.predict(X_test)
 
+#NB EVALUATION
+from sklearn import metrics
+print('Accuracy:',metrics.accuracy_score(y_test,y_pred))
 
+#Preliminary study suggested a result with 0.778 accuracy, assuming random accuracy is .5 and 1 is perfect prediction, 0.778 is moderate.
 
+#Issue encountered
+#Age is continuous variables, which won't work well with NB
+##has to convert into categorical variable by quartile differences
+##Removed Variables may contained useful information. Find a way to use it
+##NAN data, assuming its not random/random, perform missing value imputation and compare result
 
-#outputdat1 = dat1['Survived']
-#inputdat1 = dat1
-#del inputdat1['Survived']
+#Fixing Age Variable
+dat1['Age'] = pd.qcut(dat1['Age'],8, labels=[1,2,3,4,5,6,7,8])
+X = dat1.copy()
+del X['Survived']
+del X['Ticket']
+del X['Cabin']
+X['Sex'].replace('male',1,inplace=True)
+X['Sex'].replace('female',2,inplace=True)
+X['Embarked'].replace('C',1,inplace=True)
+X['Embarked'].replace('Q',2,inplace=True)
+X['Embarked'].replace('S',3,inplace=True)
+y = dat1['Survived']
+
+#2nd NB MAIN CODE and Test/train split 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.5, random_state=0)
+gnb = GaussianNB()
+y_pred = gnb.fit(X_train, y_train)
+y_pred = gnb.predict(X_test)
+
+#2nd NB EVALUATION
+from sklearn import metrics
+print('Accuracy:',metrics.accuracy_score(y_test,y_pred))
+
+#Fixing NAN value
